3e2a2205572bb45526d1467f785ce3c0e8908fc6
Yuri Reis <yuri.reis@msn.com>
2017-01-17 17:29:16 -0200

    Set environment variables correctly

--- duolingo_bot.py
+++ duolingo_bot.py
@@ -1,22 +1,20 @@
-#!/home/yurireis/env/duolingo/bin python3
 from datetime import datetime
-
 import os
-
 from duolingo import Duolingo
 from socialbase import SocialBase
 from support import get_list_formmated
 
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD_NAME')
+SB_USERNAME = os.getenv('SB_USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+DUO_USERNAME = os.getenv('DUO_USERNAME')
 NETWORK = os.getenv('NETWORK_NAME')
 APP = os.getenv('APP')
 API = os.getenv('API')
 
 today = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
 leaderboard = {}
-lingo = Duolingo('sb_challenge', 'socialbase')
-base = SocialBase(USERNAME, PASSWORD, NETWORK, APP, API)
+lingo = Duolingo(DUO_USERNAME, PASSWORD)
+base = SocialBase(SB_USERNAME, PASSWORD, NETWORK, APP, API)
 leaderboard['week'] = lingo.get_leaderboard('week', today)
 leaderboard['month'] = lingo.get_leaderboard('month', today)
 post = get_list_formmated(leaderboard['week'])
